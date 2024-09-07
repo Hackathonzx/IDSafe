13ff3675bf3f0ff70f7f1a18eaec6c2ee513efb2
nathfavour <116535483+nathfavour@users.noreply.github.com>
2024-09-07 14:20:42 +0000

    added another docs to the place

--- lib/main.dart
+++ lib/main.dart
@@ -154,34 +154,32 @@ class _HomePageState extends State<HomePage>
                 'Instantly deploy your Application with IDSafe SDK.',
                 'https://vercel.com/new',
               ),
-              if (MediaQuery.of(context).size.width < 600)
-                NavigationBar(
-                  destinations: [
-                    NavigationDestination(
-                      icon: Icon(Icons.home),
-                      label: 'Home',
-                    ),
-
-// Cut out continuation.
-
-                    NavigationDestination(
-                      icon: Icon(Icons.token),
-                      label: 'Token',
-                    ),
-                    NavigationDestination(
-                      icon: Icon(Icons.info),
-                      label: 'Info',
-                    ),
-                    NavigationDestination(
-                      icon: Icon(Icons.account_circle),
-                      label: 'Account',
-                    ),
-                  ],
-                )
             ],
           ),
         ),
       ),
+      bottomNavigationBar: MediaQuery.of(context).size.width < 600
+          ? NavigationBar(
+              destinations: [
+                NavigationDestination(
+                  icon: Icon(Icons.home),
+                  label: 'Home',
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.token),
+                  label: 'Token',
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.info),
+                  label: 'Info',
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.account_circle),
+                  label: 'Account',
+                ),
+              ],
+            )
+          : null,
     );
   }
 
@@ -228,6 +226,267 @@ class _HomePageState extends State<HomePage>
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:myapp/connect/connect.dart';
+// import 'package:myapp/mint/mint.dart';
+// import 'package:myapp/screens/fourth.dart';
+// import 'package:myapp/screens/second.dart';
+// import 'package:myapp/screens/third.dart';
+
+// void main() {
+//   String arg = const String.fromEnvironment('simple'); // arg = "example"
+
+//   runApp(IDSafeApp());
+// }
+
+// class IDSafeApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'IDSafe',
+//       theme: ThemeData(
+//         primarySwatch: Colors.deepPurple,
+//         brightness: Brightness.dark,
+//       ),
+//       home: HomePage(),
+//     );
+//   }
+// }
+
+// class HomePage extends StatefulWidget {
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
+
+// class _HomePageState extends State<HomePage>
+//     with SingleTickerProviderStateMixin {
+//   late AnimationController _controller;
+//   late Animation<double> _animation;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _controller = AnimationController(
+//       duration: const Duration(seconds: 2),
+//       vsync: this,
+//     )..repeat(reverse: true);
+//     _animation = CurvedAnimation(
+//       parent: _controller,
+//       curve: Curves.easeInOut,
+//     );
+//   }
+
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('IDSafe'),
+//         backgroundColor: Colors.green,
+//       ),
+//       drawer: MediaQuery.of(context).size.width >= 600
+//           ? Drawer(
+//               child: ListView(
+//                 padding: EdgeInsets.zero,
+//                 children: [
+//                   const DrawerHeader(
+//                     decoration: BoxDecoration(
+//                       color: Colors.green,
+//                     ),
+//                     child: Text(' Menu'),
+//                   ),
+//                   ListTile(
+//                     title: const Text('Home'),
+//                     onTap: () {
+//                       // Handle navigation to home
+//                       Navigator.pop(context);
+//                     },
+//                   ),
+//                   ListTile(
+//                     title: const Text('Token'),
+//                     onTap: () {
+//                       // Handle navigation to token
+
+//                       Navigator.pop(context);
+//                     },
+//                   ),
+//                   ListTile(
+//                     title: const Text('Info'),
+//                     onTap: () {
+//                       // Handle navigation to info
+//                       Navigator.pop(context);
+//                     },
+//                   ),
+//                   ListTile(
+//                     title: const Text('Account'),
+//                     onTap: () {
+//                       // Handle navigation to account
+//                       Navigator.pop(context);
+//                     },
+//                   ),
+//                 ],
+//               ),
+//             )
+//           : null,
+//       body: SingleChildScrollView(
+//         child: Padding(
+//           padding: const EdgeInsets.all(16.0),
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.center,
+//             children: [
+//               FadeTransition(
+//                 opacity: _animation,
+//                 child: Image.asset(
+//                   'assets/logo.png',
+//                   width: 720,
+//                   height: 160,
+//                 ),
+//               ),
+//               SizedBox(height: 20),
+//               Text(
+//                 ' Connect Wallet',
+//                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+//               ),
+//               SizedBox(height: 20),
+//               _buildInputField('Address', Icons.email),
+//               SizedBox(height: 20),
+//               ElevatedButton(
+//                 onPressed: () {
+//                   // Handle form submission
+//                 },
+//                 child: Text('Connect'),
+//               ),
+//               SizedBox(height: 20),
+//               _buildLinkCard(
+//                 'Docs',
+//                 'Find in-depth information about IDSafe features and API.',
+//                 'https://nextjs.org/docs',
+//               ),
+//               _buildLinkCard(
+//                 'Learn',
+//                 'Learn how to protect your privacy with IDSafe in an interactive course with quizzes!',
+//                 'https://nextjs.org/learn',
+//               ),
+//               _buildLinkCard(
+//                 'Templates',
+//                 'Explore starter templates for IDSafe connections.',
+//                 'https://vercel.com/templates',
+//               ),
+//               _buildLinkCard(
+//                 'Deploy',
+//                 'Instantly deploy your Application with IDSafe SDK.',
+//                 'https://vercel.com/new',
+//               ),
+//               if (MediaQuery.of(context).size.width < 600)
+//                 NavigationBar(
+//                   destinations: [
+//                     NavigationDestination(
+//                       icon: Icon(Icons.home),
+//                       label: 'Home',
+//                     ),
+
+// // Cut out continuation.
+
+//                     NavigationDestination(
+//                       icon: Icon(Icons.token),
+//                       label: 'Token',
+//                     ),
+//                     NavigationDestination(
+//                       icon: Icon(Icons.info),
+//                       label: 'Info',
+//                     ),
+//                     NavigationDestination(
+//                       icon: Icon(Icons.account_circle),
+//                       label: 'Account',
+//                     ),
+//                   ],
+//                 )
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+
+//   Widget _buildInputField(String label, IconData icon) {
+//     return Padding(
+//       padding: const EdgeInsets.symmetric(vertical: 10.0),
+//       child: TextFormField(
+//         decoration: InputDecoration(
+//           labelText: label,
+//           prefixIcon: Icon(icon),
+//           border: OutlineInputBorder(
+//             borderRadius: BorderRadius.circular(10),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+
+//   Widget _buildLinkCard(String title, String description, String url) {
+//     return Card(
+//       margin: EdgeInsets.symmetric(vertical: 10),
+//       child: InkWell(
+//         onTap: () {
+//           // Handle link tap
+//         },
+//         child: Padding(
+//           padding: const EdgeInsets.all(16.0),
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               Text(
+//                 title,
+//                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+//               ),
+//               SizedBox(height: 5),
+//               Text(
+//                 description,
+//                 style: TextStyle(fontSize: 16, color: Colors.green[400]),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 
 
