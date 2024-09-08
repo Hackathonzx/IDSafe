16f7e69270052b1cadf3547aecb41a2666eb8a04
nathfavour <116535483+nathfavour@users.noreply.github.com>
2024-09-08 17:20:15 +0400

    added jaspr and serverpod

--- idsafeweb/lib/app.dart
+++ idsafeweb/lib/app.dart
@@ -6,9 +6,6 @@ import 'pages/about.dart';
 import 'pages/home.dart';
 
 // The main component of your application.
-//
-// By using multi-page routing, this component will only be built on the server during pre-rendering and
-// **not** executed on the client. Instead only the nested [Home] and [About] components will be mounted on the client.
 class App extends StatelessComponent {
   const App({super.key});
 
@@ -22,28 +19,18 @@ class App extends StatelessComponent {
 
     // Renders a <div class="main"> html element with children.
     yield div(classes: 'main', [
-      const Header(),
       Router(routes: [
-        Route(path: '/', title: 'Home', builder: (context, state) => const Home()),
-        Route(path: '/about', title: 'About', builder: (context, state) => const About()),
+        ShellRoute(
+          builder: (context, state, child) => Fragment(children: [
+            const Header(),
+            child,
+          ]),
+          routes: [
+            Route(path: '/', title: 'Home', builder: (context, state) => const Home()),
+            Route(path: '/about', title: 'About', builder: (context, state) => const About()),
+          ],
+        ),
       ]),
     ]);
   }
-
-  // Defines the css styles for elements of this component.
-  //
-  // By using the @css annotation, these will be rendered automatically to css inside the <head> of your page.
-  // Must be a variable or getter of type [List<StyleRule>].
-  @css
-  static final styles = [
-    css('.main', [
-      // The '&' refers to the parent selector of a nested style rules.
-      css('&').box(height: 100.vh).flexbox(direction: FlexDirection.column, wrap: FlexWrap.wrap),
-      css('section').flexItem(flex: Flex(grow: 1)).flexbox(
-        direction: FlexDirection.column,
-        justifyContent: JustifyContent.center,
-        alignItems: AlignItems.center,
-      ),
-    ]),
-  ];
 }
--- idsafeweb/lib/components/counter.dart
+++ idsafeweb/lib/components/counter.dart
@@ -1,6 +1,5 @@
 import 'package:jaspr/jaspr.dart';
 
-import '../constants/theme.dart';
 import 'flutter_counter.dart' if (dart.library.io) 'flutter_counter_fallback.dart';
 
 class Counter extends StatefulComponent {
@@ -38,26 +37,4 @@ class CounterState extends State<Counter> {
       },
     );
   }
-
-  @css
-  static final styles = [
-    css('.counter', [
-      css('&').flexbox(alignItems: AlignItems.center).box(
-            padding: EdgeInsets.symmetric(vertical: 10.px),
-            border: Border.symmetric(vertical: BorderSide.solid(color: primaryColor, width: 1.px)),
-          ),
-      css('button', [
-        css('&')
-            .text(fontSize: 2.rem)
-            .box(width: 2.em, height: 2.em, border: Border.unset, cursor: Cursor.pointer)
-            .box(radius: BorderRadius.all(Radius.circular(2.em)))
-            .flexbox(justifyContent: JustifyContent.center, alignItems: AlignItems.center)
-            .background(color: Colors.transparent),
-        css('&:hover').background(color: const Color.hex('#0001')),
-      ]),
-      css('span') //
-          .box(padding: EdgeInsets.symmetric(horizontal: 2.rem), boxSizing: BoxSizing.borderBox, minWidth: 2.5.em)
-          .text(color: primaryColor, fontSize: 4.rem, align: TextAlign.center),
-    ]),
-  ];
 }
